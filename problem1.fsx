4ed1353b61708efdb0b1a80de2db622177966690
alveflo <victor.alveflo@gmail.com>
2021-06-24 22:44:48 +0200

    Add problem1

---
+++ problem1.fsx
@@ -0,0 +1,16 @@
+﻿// If we list all the natural numbers below 10
+// that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+// The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+let rec test num sum =
+    if num > 999 then sum
+    else
+        if num % 3 = 0 || num % 5 = 0 then
+            test (num + 1) (sum + num)
+        else
+            test (num + 1) sum
+
+let res = test 1 0
+printfn "%d" res
